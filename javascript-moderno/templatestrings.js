35a8b1dd2b386f9dd344d51fcad548b6243cd7b5
Jhonatan <jhonatanruivo@outlook.com>
2024-01-26 22:06:30 -0300

    feat: module modern javascript - templatestrings

---
+++ javascript-moderno/templatestrings.js
@@ -0,0 +1,11 @@
+// ----------------------------------- TEMPLATE STRINGS -------------------------------------------------
+// Template strings é uma maneira mais fácil de concatenar strings com código javascript como variáveis e objetos ou 
+// até mesmo funções
+
+const firstName = 'Jhonatan' 
+const lastName = 'Ruivo'
+
+console.log(`Meu nome completo é ${firstName} de Oliveira ${lastName}`); // Usamos a acentuação de crase para encapsular o
+                                                                         // conteúdo e dentro deste encapsulamento usamos
+                                                                         // o cifrão com chaves => ${} e dentro atribuímos
+                                                                         // o Javascript.
