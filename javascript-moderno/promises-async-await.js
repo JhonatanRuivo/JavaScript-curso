2380dcbea71ef4bc2237ab58972076e92e3967d1
Jhonatan <jhonatanruivo@outlook.com>
2024-01-27 11:29:23 -0300

    feat: async await in javascript

--- javascript-moderno/promises-async-await.js
+++ javascript-moderno/promises-async-await.js
@@ -1,18 +1,19 @@
 // ----------------------------------- PROMISES - ASYNC - AWAIT ----------------------------------------------
-// PROMISE
 
 // A promise é uma função que me retorna uma promessa que pode ser que ela foi resolvida ou foi rejeitada
 {
     const testPromise = function() {
         return new Promise((resolve, reject) => {   // Aqui declaramos uma fallback dentro da promise com um arrow function, com dois parâmetros 
                                                     // o resolve e o reject.
-            let error = false                       
+            setTimeout(()=>{
+                let error = false                       
 
             if(error) {
                 reject("Não foi possível realizar a operação!") // Se a variável error for true então ela não será resolvida e cairá no parâmetro reject 
             } else {
                 resolve("Promessa devolvida com sucesso!")      // Porém se o erro for false a promessa será resolvida e cairá no parâmetro resolve.       
             }
+            }, 2000)
         })
     }
 
@@ -21,4 +22,17 @@
     }).catch((err)=>{             // e teremos o método catch para pegar o erro da promise caso ela não renha sido resolvida e aí poderemos manipular como  
         alert(err)                // acharmos melhor esses dois resultados.
     })
+
+
+    async function asyncFunctionExemple() {   // Com a função async teremos dentro dela agora o método await, este método fará com que o restante da 
+        await testPromise().then((res)=> {    // da função execute somente depois que a função testPromise() que está no await for executado.
+            alert(res)
+        }).catch((err)=> {
+            alert(err)
+        })                   
+        alert('Olá!')                         
+    }
+
+    asyncFunctionExemple()
 }
+
