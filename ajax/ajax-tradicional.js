c6a61185880c548d255486bc34694b5bc7ba0f7f
Jhonatan <jhonatanruivo@outlook.com>
2024-01-28 18:26:09 -0300

    feat: add method patch in ajax with fetch

--- ajax/ajax-tradicional.js
+++ ajax/ajax-tradicional.js
@@ -26,7 +26,7 @@ fetch('https://jsonplaceholder.typicode.com/posts', {
 .then(Response => Response.json())                      // A promessa é resolvida para o objeto Response
 .then(json => console.log(json))
 
-// Método patch para atualizar algum dado pela requisição
+// Método PUT para atualizar algum dado pela requisição
 fetch('https://jsonplaceholder.typicode.com/posts/1', {
   method: 'PUT',
   body: JSON.stringify({
@@ -34,6 +34,19 @@ fetch('https://jsonplaceholder.typicode.com/posts/1', {
     title: 'foo',
     body: 'bar',
     userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+  // Método PATCH 
+  fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: 'PATCH',
+  body: JSON.stringify({
+    title: 'changed',
   }),
   headers: {
     'Content-type': 'application/json; charset=UTF-8',
